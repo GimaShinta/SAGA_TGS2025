ab41fab371371dbb4c0638e8b5ef39cf1b13369c
manato444 <138412544+manato444@users.noreply.github.com>
2025-04-21 13:48:59 +0900

    スタート・タイトルシーンの作成
    2025/4/21

--- Scene/SceneType/StartScene.cpp
+++ Scene/SceneType/StartScene.cpp
@@ -1,1 +1,43 @@
 #include "StartScene.h"
+
+StartScene::StartScene()
+{
+}
+
+StartScene::~StartScene()
+{
+}
+
+void StartScene::Initialize()
+{
+}
+
+eSceneType StartScene::Update(float delta_second)
+{
+	// 入力クラスのインスタンスを取得
+	InputManager* input = Singleton<InputManager>::GetInstance();
+
+	// Aボタンでゲームメインへ遷移
+	if (input->GetKeyDown(KEY_INPUT_SPACE) ||
+		input->GetButtonDown(XINPUT_BUTTON_A))
+	{
+		return eSceneType::eTitle;
+	}
+
+	// 現在のシーンを維持する
+	return GetNowSceneType();
+}
+
+void StartScene::Draw()
+{
+	DrawString(0, 0, "Start", GetColor(255, 255, 255));
+}
+
+void StartScene::Finalize()
+{
+}
+
+eSceneType StartScene::GetNowSceneType() const
+{
+	return eSceneType::eStart;
+}
--- Scene/SceneType/StartScene.h
+++ Scene/SceneType/StartScene.h
@@ -3,5 +3,32 @@
 class StartScene :
     public SceneBase
 {
+public:
+	StartScene();
+	virtual ~StartScene();
+
+public:
+	// 初期化処理
+	virtual void Initialize() override;
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	/// <param name="delta_second">１フレーム当たりの時間</param>
+	/// <returns></returns>
+	virtual eSceneType Update(float delta_second) override;
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	/// <returns></returns>
+	virtual void Draw() override;
+
+	// 終了時処理（使ったインスタンスの削除とか）
+	virtual void Finalize() override;
+
+	// 現在のシーンタイプ（オーバーライド必須）
+	virtual eSceneType GetNowSceneType() const override;
+
 };
 
