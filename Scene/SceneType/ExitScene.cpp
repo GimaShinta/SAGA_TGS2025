556a68876bf410d2f519a3a70ba2f31b73d09a9d
manato444 <138412544+manato444@users.noreply.github.com>
2025-06-04 14:48:45 +0900

    敵の弾画像

---
+++ Scene/SceneType/ExitScene.cpp
@@ -0,0 +1,34 @@
+#include "ExitScene.h"
+
+ExitScene::ExitScene()
+{
+}
+
+
+ExitScene::~ExitScene()
+{
+}
+
+void ExitScene::Initialize()
+{
+}
+
+eSceneType ExitScene::Update(float delta_second)
+{
+	return eSceneType();
+}
+
+void ExitScene::Draw()
+{
+}
+
+void ExitScene::Finalize()
+{
+}
+
+eSceneType ExitScene::GetNowSceneType() const
+{
+	return eSceneType::eXit;
+}
+
+
